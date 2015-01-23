0077c02b8c435f34467fd8f8d3559317ed5ec432
EricSchles <ericschles@gmail.com>
2015-01-23 07:48:35 -0800

    updated

--- cron.py
+++ cron.py
@@ -3,7 +3,14 @@ import time
 from sys import argv
 while True:
 
-    run()
+    msg = argv[1]
+    on_heroku = False
+    try:
+        msg = argv[2]
+        on_heroku = True
+    except:
+        run(first=msg,second=on_heroku)
+    run(first=msg,second=on_heroku)
     if not argv[1]:
         time.sleep(300) #sleep for five minutes
     else:
