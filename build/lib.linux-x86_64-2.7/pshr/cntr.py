faa22d04c2792e5b6dc26d4584d1bb9b67942ea0
Eric Schles <ericschles@gmail.com>
2016-09-15 08:55:15 -0400

    upgrading for compatibility with python3

--- build/lib.linux-x86_64-2.7/pshr/cntr.py
+++ build/lib.linux-x86_64-2.7/pshr/cntr.py
@@ -62,9 +62,9 @@ def counter(extension,roots=[]):
                     month_totals[month].total_len += file_len(file) #per month
                     total_len += file_len(file) #total
 
-    print("You worked best between",str(ave_hour/num_files),"and",str((ave_hour/num_files)+1) ,"so far")
-    print("You've written", total_len ,"lines so far.")
-    print() 
+    print "You worked best between %s and %s so far" % (str(ave_hour/num_files),str((ave_hour/num_files) + 1))
+    print "You've written %d lines so far." % total_len
+    print 
 
     month_translate = {
         "01":"january",
@@ -83,15 +83,14 @@ def counter(extension,roots=[]):
 
 
     for month in month_totals:
-        print("For",month_translate[month])
+        print "For",month_translate[month]
         if month_totals[month].num_files != 0:
             month_totals[month].ave_hour = month_totals[month].ave_hour/month_totals[month].num_files
         else:
             month_totals[month].ave_hour = 0
-        print("You worked best between", (str(month_totals[month].ave_hour))," and ",str(month_totals[month].ave_hour +1) ," so far.")
-        
-        print("You've written",month_totals[month].total_len," lines so far.")
-        print() 
+        print "You worked best between %s and %s so far" % (str(month_totals[month].ave_hour),str(month_totals[month].ave_hour + 1))
+        print "You've written %d lines so far." % (month_totals[month].total_len)
+        print 
                 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
--- build/lib.linux-x86_64-2.7/pshr/pusher.py
+++ build/lib.linux-x86_64-2.7/pshr/pusher.py
@@ -2,8 +2,10 @@ import time
 import argparse
 from subprocess import call
 from tooling import *
+from cntr import counter
+import os
 
-def main(msg,on_heroku=False,sleep_for=300,continuous=False):
+def main(msg,on_heroku=False,sleep_for=300,continuous=False,analysis=False,extension=".py",roots=[os.getcwd()]):
     while True:
         call(["git","add","--all",":/"])
         call(["git","commit","-m",msg])
@@ -11,6 +13,8 @@ def main(msg,on_heroku=False,sleep_for=300,continuous=False):
         if on_heroku:
             call(["git","push","heroku","master"])
 
+        if analysis:
+            counter(extension,roots=roots)
         if continuous:
             time.sleep(sleep_for) #sleep for n seconds
         else:
@@ -23,10 +27,9 @@ if __name__ == '__main__':
     parser.add_argument("--push-n-seconds",type=int, help="how long to wait between pushes in seconds")
     parser.add_argument("--continuous",type=bool,help="run this script continously")
     parser.add_argument("--generate-gitignore",type=bool,help="generate a gitignore for your code")
+    parser.add_argument("--analysis",type=bool,help="Turns on analysis, which analyzes how much code you've written for the current project")
     args = parser.parse_args()
     if args.generate_gitignore: generate_gitignore()
-    main(msg=args.message,on_heroku=args.on_heroku,sleep_for=args.push_n_seconds)
-    
-        
+    main(msg=args.message,on_heroku=args.on_heroku,sleep_for=args.push_n_seconds,analysis=args.analysis)
 
 
